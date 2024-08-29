c2cc160b7079a6d474a657f5df558d86c4565f36
Gustavo René <renegustavo42@gmail.com>
2024-08-29 18:58:27 -0300

    Curso de testes iniciado

---
+++ tests/curso.api.tests/Integrations/Controller/UsuarioControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Bogus.Extensions;
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace curso.api.tests.Integrations.Controller
+{
+    public class UsuarioControllerTests
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpClient;
+
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+
+        //WhenGivenThen
+        [Fact]
+        public void Logar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso()
+        {
+            // Arrange
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = "gustavorene",
+                Senha = "123456"
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput));
+
+            // Act
+            var httpClientRequest = _httpClient.PostAsync("api/v1/usuario/logar", content).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, httpClientRequest.StatusCode);
+        }
+
+
+    }
+}
