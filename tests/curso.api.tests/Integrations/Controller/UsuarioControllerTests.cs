6e1f0d1cae9148caeb0e90863b28122c5825d4fb
Gustavo René <renegustavo42@gmail.com>
2024-08-29 21:31:16 -0300

    Criando os testes de requisição de login e registro, afim de obter as validações necessárias

--- tests/curso.api.tests/Integrations/Controller/UsuarioControllerTests.cs
+++ tests/curso.api.tests/Integrations/Controller/UsuarioControllerTests.cs
@@ -3,20 +3,25 @@ using curso.api.Models.Usuarios;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using System.Net;
+using System.Text;
 using System.Net.Http;
 using Xunit;
+using Xunit.Abstractions;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace curso.api.tests.Integrations.Controller
 {
-    public class UsuarioControllerTests
+    public class UsuarioControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
 
         private readonly WebApplicationFactory<Startup> _factory;
         private readonly HttpClient _httpClient;
+        private readonly ITestOutputHelper _output;
 
-        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory, ITestOutputHelper output)
         {
             _factory = factory;
+            _output = output;
             _httpClient = _factory.CreateClient();
         }
 
@@ -31,15 +36,39 @@ namespace curso.api.tests.Integrations.Controller
                 Senha = "123456"
             };
 
-            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput));
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput), Encoding.UTF8, "application/json");
 
             // Act
             var httpClientRequest = _httpClient.PostAsync("api/v1/usuario/logar", content).GetAwaiter().GetResult();
 
+            var loginViewModelOutput = JsonConvert.DeserializeObject<LoginViewModelOutput>(httpClientRequest.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
             // Assert
             Assert.Equal(HttpStatusCode.OK, httpClientRequest.StatusCode);
+            Assert.NotNull(loginViewModelOutput.Token);
+            Assert.Equal(loginViewModelInput.Login, loginViewModelOutput.Usuario.Login);
+            _output.WriteLine(loginViewModelOutput.Token);
         }
 
+        [Fact]
+        public void Registrar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso()
+        {
+            // Arrange
+            var registroViewModelInput = new RegistroViewModelInput
+            {
+                Login = "gustavorene",
+                Email = "gustavo@gmail.com",
+                Senha = "123456"
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(registroViewModelInput), Encoding.UTF8, "application/json");
+
+            // Act
+            var httpClientRequest = _httpClient.PostAsync("api/v1/usuario/registrar", content).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, httpClientRequest.StatusCode);
+        }
 
     }
 }
